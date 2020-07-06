8c962d5fbb7f119c9406c002fe26a4e6ca5126c9
ym781020 <ym781020@163.com>
2020-07-06 10:16:40 +0800

    登录组件的配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Login from '@/components/login/login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/'
-    }
+    { name: 'login', path: '/login', component: Login }    
   ],
   mode: 'history'
 })
